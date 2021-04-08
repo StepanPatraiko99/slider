90bb61e18b2e53f113708b891ff41a8d1ac8f625
StepanPatraiko99 <82183011+StepanPatraiko99@users.noreply.github.com>
2021-04-09 00:59:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+[
+    {
+       "id":1,
+       "title":"Toyota Prius",
+       "ops":"2009 рік випуску",
+       "opss":"Ціна авто 10000$`\r\n",
+       "engine":"Ціна аналогічно в україні 14000$",
+       "img":"https://upload.wikimedia.org/wikipedia/commons/5/54/2019_Toyota_Prius_Business_Edition%2B_PHEV_1.8.jpg"
+    },
+    {
+       "id":3,
+       "title":"Ваз 1010",
+       "ops":"Таваівфівфівффів",
+       "opss":"sadjasdasdsa",
+       "engine":"asdasdasda",
+       "img":"https://motor.ru/thumb/2250x0/filters:quality(75):no_upscale()/imgs/2020/02/12/10/3775229/e85849494c906f66b9a3597ceb33f7ba230c758e.jpg"
+    },
+    {
+       "id":4,
+       "title":"Ваз 1010",
+       "ops":"Таваівфівфівффів",
+       "opss":"sadjasdasdsa",
+       "engine":"asdasdasda",
+       "img":"https://motor.ru/thumb/2250x0/filters:quality(75):no_upscale()/imgs/2020/02/12/10/3775229/e85849494c906f66b9a3597ceb33f7ba230c758e.jpg"
+    }
+ ]
